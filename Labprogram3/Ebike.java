de80217e662b092f90504cc10db7abef3dc800b4
Greeshma <greeshmakulal212@gmail.com>
2025-09-09 22:06:23 +0530

    Create Ebike.java

---
+++ Labprogram3/Ebike.java
@@ -0,0 +1,15 @@
+package transport;
+public class Ebike extends Bicycle
+{
+    private int battery;
+	public Ebike(String id, int battery){
+	      super (id);
+	      this.battery=battery;
+	      System.out.println("Ebike() constructor is called");
+	}
+	@Override
+	public void deliver(String item, String place) {
+		System.out.println("checking battery"+battery+"%");
+		super.deliver(item, place);
+	}
+}
