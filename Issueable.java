84401060115f91a2415f680a2ce7bb9ecc76df33
Greeshma <greeshmakulal212@gmail.com>
2025-09-12 23:21:08 +0530

    Add files via upload

---
+++ Issueable.java
@@ -0,0 +1,14 @@
+package library_management_system;
+
+
+public interface Issueable {
+		
+		// To issues  a book 
+		    void issuebook();       
+		 // To return a book
+		   void returnBook();   
+		// To check availability of the book
+		boolean isAvailable();                
+	}
+
+
